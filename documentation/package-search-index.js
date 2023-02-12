ad6e35e43bb2c819fcf71fca95d27cc7514f0364
Marcin Knyc <maroxesen@gmail.com>
2023-02-12 01:48:46 +0100

    feat: f5: documentation regenerated.

--- documentation/package-search-index.js
+++ documentation/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.polsl.stylometry.controller"},{"l":"com.polsl.stylometry.model"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.polsl.stylometry.controller"},{"l":"com.polsl.stylometry.entities"},{"l":"com.polsl.stylometry.model"}];updateSearchResults();
